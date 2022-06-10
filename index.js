9b5c95986eae25fe6e76eab1727ce0a6ab624c63
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-10 08:42:57 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+function findMatching(drivers,value){
+    return drivers.filter(function(driver){ 
+        return driver.toLowerCase() === value.toLowerCase()
+     })
+  }
+  
+function fuzzyMatch(drivers,value){
+    return drivers.filter(function(driver){ 
+        return driver.toLowerCase().substring(0,value.length) ===value.toLowerCase() 
+    })
+  }
+  
+function matchName(drivers,argument){
+    return drivers.filter(function(driver){ 
+        return driver.name === argument
+     })
+  }
+
+
+
